5afae231210083128bbf3b1b434710d05251144f
Frantisek Harnicar <frantisek.harnicar@student.fei.tuke.sk>
2023-02-16 08:30:51 +0100

    Routing

---
+++ shoping-list/src/router/index.js
@@ -0,0 +1,33 @@
+import {createRouter, createWebHistory} from "vue-router"
+import SearchView from "../views/Search.vue"
+import CartView from "../views/Cart.vue"
+import FavouriteView from "../views/Favourite.vue"
+import MealView from "../views/Meal.vue"
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "search",
+            component: SearchView
+        },
+        {
+            path: "/cart",
+            name: "cart",
+            component: CartView
+        },
+        {
+            path: "/favourite",
+            name: "favourite",
+            component: FavouriteView
+        },
+        {
+            path: "/meal/:id",
+            name: "meal",
+            component: MealView
+        },
+    ]
+})
+
+export default router
---
+++ shoping-list/src/views/Meal.vue
@@ -0,0 +1,16 @@
+<script>
+    import { useRoute } from 'vue-router';
+    import {ref, onBeforeMount} from 'vue'
+
+    const car = ref(null)
+    const route = useRoute()
+    const {id} = route.params
+
+    onBeforeMount(() => {
+        // porovnanie id
+    })
+</script>
+
+<template>
+
+</template>
